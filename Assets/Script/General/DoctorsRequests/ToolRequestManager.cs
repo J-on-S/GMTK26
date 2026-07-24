090985a5a8df0db72dbcbaafc1f4967878b288c4
murashkii <isabellamurashov@gmail.com>
2026-07-24 18:09:04 -0400

    fixed randomizing and body parts addition

--- Assets/Script/General/DoctorsRequests/ToolRequestManager.cs
+++ Assets/Script/General/DoctorsRequests/ToolRequestManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEditor;
 
 public class ToolRequestManager : MonoBehaviour
 {
@@ -15,8 +16,10 @@ public class ToolRequestManager : MonoBehaviour
     }
 
     // request stuff
-    public List<ToolRequest> availableRequests = new List<ToolRequest>();
+    public List<ToolRequest> availableRequests = new List<ToolRequest>();   // this contains the active doctor's requests
+    public List<ToolRequest> allTools = new List<ToolRequest>();            // this contains the list of ALL TOOLS that will be used to randomly select items for the doctor's requests
     public float timeBetweenRequests = 5f; // oooldown before next order from doctor
+    public float numberOfRequests = 5;  // number of total requests that the doctor will ask for
 
     // states
     private enum State{Idle, ActiveRequest, Cooldown}
@@ -31,6 +34,7 @@ public class ToolRequestManager : MonoBehaviour
     {
         // start a request immediately
         StartCooldown();
+        FinishDoctorRequestList(); // add random assortment of tools to doctor request list -- this will probably need to be called after the client commands are in
     }
 
     // Update is called once per frame
@@ -51,7 +55,31 @@ public class ToolRequestManager : MonoBehaviour
                 // just idle
                 break;
         }
+    }
 
+    // PHILIPPE CALLS THIS FUNCTION
+    // this can be called in the client request code (philippe's part) to add the body parts to the list
+    public void AddToDoctorRequestList(string bodyPartName)
+    {
+        availableRequests.Add(new ToolRequest
+        {
+            itemName = bodyPartName,
+            itemType = ItemType.BodyPart,
+            timeLimit = UnityEngine.Random.Range(6f, 9f)    // random time limit between 6-9 seconds
+        });
+    }
+
+    // PHILIPPE CALLS THIS FUNCTION PROBABLY ALSO -- after all body parts have been added to the list
+    public void FinishDoctorRequestList()
+    {
+        if (availableRequests.Count >= numberOfRequests) return; // request list already full no need to add anything else
+        
+        // fill up the available requests list with random tools from the list of all tools
+        while (availableRequests.Count < numberOfRequests)
+        {
+            int choice = Random.Range(0, allTools.Count);       // random index of tool
+            availableRequests.Add(allTools[choice]);
+        }
 
     }
 
@@ -62,8 +90,7 @@ public class ToolRequestManager : MonoBehaviour
         if (remainingTime <= 0)
             {
                 FailRequest();
-            }
-        
+            }   
     }
 
     // cooldown between requests
@@ -79,7 +106,7 @@ public class ToolRequestManager : MonoBehaviour
     // randomize the requests
     public void StartNewRandomRequest()
     {
-        if (availableRequests.Count == 0) return;   // maybe change to a set number of requests? list should always have all the tools
+        if (availableRequests.Count == 0) return;   // this is the scenario when the doctor requests are done, something needs to happen here
 
         // get a random item from the list
         int index = Random.Range(0, availableRequests.Count);
@@ -90,6 +117,10 @@ public class ToolRequestManager : MonoBehaviour
         string itemCategory = currentRequest.itemType.ToString();
         Debug.Log($"Hey, hand me a {itemCategory}: [{currentRequest.itemName}] within {remainingTime:F1} seconds!");
         //Debug.Log($"Hey, hand me a: {currentRequiredTool} within {remainingTime:F1} seconds!");
+
+        // remove request from the list immediately, doctor will not ask again regardless of if request is fulfilled or not
+        availableRequests.Remove(currentRequest);
+        Debug.Log($"This is how many items are in the list {availableRequests.Count}");
 
     }
 
@@ -102,6 +133,7 @@ public class ToolRequestManager : MonoBehaviour
         {
             Debug.Log("Dude thanks for giving me that.");
             StartCooldown();
+
             return true;    // success
 
             // some sort of score stuff
