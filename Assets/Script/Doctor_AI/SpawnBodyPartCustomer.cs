446e97f2000c3b5be6912450cd833e8db95d576c
AcacieS <acaciesong@gmail.com>
2026-07-24 21:41:05 -0400

    customer spawn body part on it, rotation of doctor towards bed

---
+++ Assets/Script/Doctor_AI/SpawnBodyPartCustomer.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBodyPartCustomer : MonoBehaviour
+{
+    [SerializeField] private BodyPartSpawn earsSpawn;
+    [SerializeField] private BodyPartSpawn nosesSpawn;
+    [SerializeField] private BodyPartSpawn legsSpawn;
+    [SerializeField] private BodyPartSpawn armsSpawn;
+    [SerializeField] private BodyPartSpawn heartSpawn;
+
+    [Header("Test")]
+    [SerializeField] private BodyPartType TestBodyPart;
+    public void Start()
+    {
+        earsSpawn.Initialize();
+        nosesSpawn.Initialize();
+        legsSpawn.Initialize();
+        armsSpawn.Initialize();
+        heartSpawn.Initialize();
+    }
+
+    [ContextMenu("AddBodyPart")]
+    public void AddBodyPartTest()
+    {
+        AddBodyPart(TestBodyPart);
+    }
+    public void AddBodyPart(BodyPartType newBodyPart)
+    {
+        switch (newBodyPart)
+        {
+            case BodyPartType.Leg:
+                RandomBodyPartPos(legsSpawn);
+                break;
+            case BodyPartType.Ear:
+                RandomBodyPartPos(earsSpawn);
+                break;
+            case BodyPartType.Arm:
+                RandomBodyPartPos(armsSpawn);
+                break;
+            case BodyPartType.Heart:
+                RandomBodyPartPos(heartSpawn);
+                break;
+            case BodyPartType.Nose:
+                RandomBodyPartPos(nosesSpawn);
+                break;
+            default:
+                Debug.LogError("Hey");
+                break;
+        }
+    }
+    public void RandomBodyPartPos(BodyPartSpawn bodyPartSpawn)
+    {
+        List<BodyPartPos> bodiesPartPos = bodyPartSpawn.bodiesPos;
+        if (CheckFullBodyPart(bodiesPartPos))
+        {
+            return;
+        }
+        int indexBodyPart = Random.Range(0, bodiesPartPos.Count);
+        BodyPartPos currentBodyPos = bodiesPartPos[indexBodyPart];
+        while (currentBodyPos.hasBodyPart)
+        {
+            indexBodyPart = Random.Range(0, bodiesPartPos.Count);
+            currentBodyPos = bodiesPartPos[indexBodyPart];
+        }
+        bodiesPartPos[indexBodyPart].hasBodyPart = true;
+        Instantiate(bodyPartSpawn.bodyPartPrefab, currentBodyPos.bodyPartPos.position, currentBodyPos.bodyPartPos.rotation, transform);
+    }
+    public bool CheckFullBodyPart(List<BodyPartPos> bodiesPartPos)
+    {
+        foreach(BodyPartPos bodyPartPos in bodiesPartPos)
+        {
+            if (!bodyPartPos.hasBodyPart)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
