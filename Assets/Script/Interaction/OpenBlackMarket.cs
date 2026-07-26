9b17b4a0e7801538012afa0574c799c2d4bb1005
SuperMary65 <skornlv@icloud.com>
2026-07-25 20:22:10 -0400

    smth

--- Assets/Script/Interaction/OpenBlackMarket.cs
+++ Assets/Script/Interaction/OpenBlackMarket.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class OpenBlackMarket : MonoBehaviour, IInteractable
 {
+  public bool UIvisible;
   public GameObject UImarket;
   public void Interact(Interactor player){
     UImarket.SetActive(true);
+    UIvisible = true;
+    Cursor.lockState = CursorLockMode.None;
+    Time.timeScale = 0f;
+  }
+  public void notVisible(){
+    UImarket.SetActive(false);
+    UIvisible = false;
+    Cursor.lockState = CursorLockMode.Locked;
+    Time.timeScale = 1f;
   }
 }
