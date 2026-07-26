6de70bd87df0c3ee9d2f079a5c34b5304639c187
AcacieS <acaciesong@gmail.com>
2026-07-26 04:36:10 -0400

    client ui

---
+++ Assets/Script/General/ClientScript/GenerateClientInfoUI.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateClientInfoUI : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> cardsParent = new List<GameObject>();
+    [SerializeField] private List<Card> cards = new List<Card>();
+    [SerializeField] private BodyParts bodyParts;
+    public void Start()
+    {
+        foreach (GameObject cardParent in cardsParent)
+        {
+            Card newCard = new Card();
+            for (int i = 0; i < cardParent.transform.childCount; i++)
+            {
+                Transform child = cardParent.transform.GetChild(i);
+                if(i == 0)
+                {
+                    newCard.text = child.GetComponent<TextMeshProUGUI>();
+                }
+                else
+                {
+                    newCard.images.Add(child.GetComponent<Image>());
+                }
+            }
+            cards.Add(newCard);
+        }
+    }
+    public void GenerateUI()
+    {
+
+        for(int i=0; i<RandomizedClientList.Instance.GeneratedTaskList.Count; i++)
+        {
+            ClientTaskQueueEntry client = RandomizedClientList.Instance.GeneratedTaskList[i];
+            ClientTask clientTask = client.Task;
+            string text = "";
+            Card card = cards[i];
+            int indexImg = 0;
+            
+            foreach(BodyPartRequest bodyPartRequest in clientTask.Requests)
+            {
+                text+="+"+bodyPartRequest.Amount+"\n";
+                BodyPart bodyPart = bodyParts.SearchBodyPart(bodyPartRequest.BodyPart);
+                
+                card.images[indexImg].sprite = bodyPart.bodyPartImg;
+            }
+            
+            card.text.text = text;
+        }
+        
+    }
+}
